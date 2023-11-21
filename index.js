8bbba7f29133016a62cc6859146d723f51b756c9
harshitha4061 <145264050+harshitha4061@users.noreply.github.com>
2023-11-21 23:22:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+    <script>
+        const userForm = document.querySelector('form');
+
+        const retrieveEntries = () => {
+            let entries = localStorage.getItem('userEntries');
+            if (entries) {
+                entries = JSON.parse(entries);
+            } else {
+                entries = [];
+            }
+            return entries;
+        };
+
+        const displayEntries = () => {
+            const entries = retrieveEntries();
+
+            const tableEntries = entries.map((entry) => {
+                const nameCell = `<td>${entry.name}</td>`;
+                const emailCell = `<td>${entry.email}</td>`;
+                const passwordCell = `<td>${entry.password}</td>`;
+                const dobCell = `<td>${entry.dob}</td>`;
+                const t_cCell = `<td>${entry.t_c ? 'Yes' : 'No'}</td>`;
+                const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${t_cCell}</tr>`;
+                return row;
+            }).join("\n");
+
+            const table = `<table>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Dob</th>
+                    <th>Accepted Terms?</th>
+                </tr>
+                ${tableEntries}
+            </table>`;
+
+            let details = document.getElementById('user-entries');
+            details.innerHTML = table;
+        };
+
+        const saveUserForm = (event) => {
+            event.preventDefault();
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            const dob = document.getElementById('dob').value;
+            const t_c = document.getElementById('acceptedTerms?').checked;
+
+            const entry = {
+                name: name,
+                email: email,
+                password: password,
+                dob: dob,
+                t_c: t_c
+            };
+            let userEntries = retrieveEntries();
+
+            userEntries.push(entry);
+
+            localStorage.setItem('userEntries', JSON.stringify(userEntries));
+            displayEntries();
+        };
+</script>
+
+        userForm.addEventListener('submit', saveUserForm);
+        displayEntries();
+        if (!localStorage.getItem('userEntries')) {
+            localStorage.setItem('userEntries', JSON.stringify([]))};
+    </script>
